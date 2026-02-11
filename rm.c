3e0ddfbc21e462982a8deb0d79a4dfad03ed5460
Gaurav Jain <gauravjain200024@gmail.com>
2026-02-11 19:30:25 +0530

    code changes for RM scheduler

--- rm.c
+++ rm.c
@@ -1,37 +1,65 @@
 #include "task.h"
 #include <limits.h>
+#include <stdio.h>
 
 static int select_rm(int time, int last) {
-    int chosen = -1, min_p = INT_MAX;
+    int chosen = -1;
+    int min_period = INT_MAX;
 
     for (int i = 0; i < job_count; i++) {
+
         if (job_list[i].arrival <= time &&
             job_list[i].remaining_time > 0) {
 
-            if (job_list[i].period < min_p ||
-               (job_list[i].period == min_p &&
-                job_list[i].last_exec_time >
-                job_list[chosen].last_exec_time)) {
-                min_p = job_list[i].period;
+            if (chosen == -1) {
                 chosen = i;
+                min_period = job_list[i].period;
+            }
+            else if (job_list[i].period < min_period) {
+                chosen = i;
+                min_period = job_list[i].period;
+            }
+            else if (job_list[i].period == min_period) {
+                // tie-breaking: most recently executed
+                if (job_list[i].last_exec_time >
+                    job_list[chosen].last_exec_time) {
+                    chosen = i;
+                }
             }
         }
     }
 
+    // If last job has same highest priority, continue it
     if (last != -1 &&
         job_list[last].remaining_time > 0 &&
-        job_list[last].period == min_p)
+        job_list[last].arrival <= time &&
+        job_list[last].period == min_period) {
         return last;
+    }
 
     return chosen;
 }
 
 void rm_scheduler(int hyperperiod) {
-    int last = -1, start = 0;
+    int last = -1;
+    int start = 0;
 
     printf("\n===== RM SCHEDULE =====\n");
 
     for (int t = 0; t < hyperperiod; t++) {
+
+        // Deadline miss check
+        for (int i = 0; i < job_count; i++) {
+            if (job_list[i].remaining_time > 0 &&
+                t >= job_list[i].abs_deadline) {
+                printf("Deadline Miss at time %d : J%d (T%d)\n",
+                       t,
+                       job_list[i].job_id,
+                       job_list[i].task_id);
+                return;
+            }
+        }
+
         int curr = select_rm(t, last);
 
         if (curr != last) {
@@ -52,10 +80,10 @@ void rm_scheduler(int hyperperiod) {
         last = curr;
     }
 
-    if (last != -1)
+    if (last != -1) {
         printf("[%d] [%d] [J%d] [T%d]\n",
                start, hyperperiod,
                job_list[last].job_id,
                job_list[last].task_id);
+    }
 }
-
